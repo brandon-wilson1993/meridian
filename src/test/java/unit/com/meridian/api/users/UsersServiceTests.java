51706381bd941e5957703b99b6f33dccf5b7910e
Brandon Wilson <bwilson1052019@gmail.com>
2025-11-29 08:59:38 -0600

    refactor user service tests

--- src/test/java/unit/com/meridian/api/users/UsersServiceTests.java
+++ src/test/java/unit/com/meridian/api/users/UsersServiceTests.java
@@ -1,117 +1,145 @@
-//package unit.com.meridian.api.users;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//
-//import com.meridian.api.errors.ResourceNotFoundException;
-//import com.meridian.api.users.*;
-//
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class UsersServiceTests {
-//
-//    private static Users author;
-//
-//    @Mock
-//    private UsersRepository usersRepository;
-//
-//    @InjectMocks private UsersService usersService = new UsersServiceImpl();
-//
-//    @BeforeAll
-//    static void beforeAll() {
-//
-//        author = new Users(123L, "Testing", "Name");
-//    }
-//
+package unit.com.meridian.api.users;
+
+import com.meridian.api.errors.ResourceNotFoundException;
+import com.meridian.api.users.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UsersServiceTests {
+
+    private static Users users;
+    private static UsersDTO usersDTO;
+
+    @Mock
+    private UsersRepository usersRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private UsersService usersService = new UsersServiceImpl();
+
+    @BeforeAll
+    static void beforeAll() {
+
+        users = new Users();
+        users.setId(123L);
+        users.setFirstName("Testing");
+        users.setLastName("Name");
+
+        usersDTO = new UsersDTO();
+        usersDTO.setId(123L);
+        usersDTO.setFirstName("Testing");
+        usersDTO.setLastName("Name");
+    }
+
+    @Test
+    void createUsers_shouldCreate_whenusersDTOIsValid() {
+
+        when(modelMapper.map(any(Users.class), eq(UsersDTO.class))).thenReturn(usersDTO);
+        when(modelMapper.map(any(UsersDTO.class), eq(Users.class))).thenReturn(users);
+        when(usersRepository.save(any(Users.class))).thenReturn(users);
+
+        UsersDTO result = usersService.createUser(modelMapper.map(users, UsersDTO.class));
+
+        assertEquals(123L, result.getId());
+        assertEquals("Testing", result.getFirstName());
+        assertEquals("Name", result.getLastName());
+
+        verify(usersRepository).save(users);
+    }
+
+    @Test
+    void deleteUsersById_shouldDelete_whenIdExists() {
+
+        when(usersRepository.existsById(123L)).thenReturn(true);
+
+        usersService.deleteUserById(123L);
+
+        verify(usersRepository).deleteById(123L);
+    }
+
+    @Test
+    void deleteUsersById_shouldNotDelete_whenIdDoesNotExists() {
+
+        when(usersRepository.existsById(12L)).thenReturn(false);
+
+        assertThrows(ResourceNotFoundException.class, () -> usersService.deleteUserById(12L));
+
+        verify(usersRepository, never()).deleteById(123L);
+    }
+
 //    @Test
-//    void createAuthor_shouldCreate_whenAuthorIsValid() {
+//    void getAllUsers_shouldReturn() {
 //
-//        when(usersRepository.save(author)).thenReturn(author);
+//        Users users1 = new Users();
+//        users1.setId(456L);
+//        users1.setFirstName("Testing");
+//        users1.setLastName("Another");
 //
-//        Users result = usersService.createUser(author);
+//        Users users2 = new Users();
+//        users2.setId(789L);
+//        users2.setFirstName("Test");
+//        users2.setLastName("Name");
 //
-//        assertEquals(123L, result.getId());
-//        assertEquals("Testing", result.getFirstName());
-//        assertEquals("Name", result.getLastName());
+//        List<Users> users = Arrays.asList(users1, users2);
 //
-//        verify(usersRepository).save(author);
-//    }
+//        when(usersRepository.findAll()).thenReturn(users);
 //
-//    @Test
-//    void deleteAuthorById_shouldDelete_whenIdExists() {
+//        List<UsersDTO> result = usersService.getAllUsers();
 //
-//        when(usersRepository.existsById(123L)).thenReturn(true);
-//
-//        usersService.deleteUserById(123L);
-//
-//        verify(usersRepository).deleteById(123L);
-//    }
-//
-//    @Test
-//    void deleteAuthorById_shouldNotDelete_whenIdDoesNotExists() {
-//
-//        when(usersRepository.existsById(12L)).thenReturn(false);
-//
-//        assertThrows(ResourceNotFoundException.class, () -> usersService.deleteUserById(12L));
-//
-//        verify(usersRepository, never()).deleteById(123L);
-//    }
-//
-//    @Test
-//    void getAllAuthors_shouldReturn() {
-//
-//        Users author1 = new Users(456L, "Testing", "Another");
-//        Users author2 = new Users(789L, "Test", "Name");
-//
-//        List<Users> authors = Arrays.asList(author, author1, author2);
-//
-//        when(usersRepository.findAll()).thenReturn(authors);
-//
-//        List<Users> result = usersService.getAllUsers();
-//
-//        assertArrayEquals(authors.toArray(), result.toArray());
+//        assertArrayEquals(users.stream()
+//                .map(user -> modelMapper.map(user, UsersDTO.class)).toArray(), result.toArray());
 //        verify(usersRepository).findAll();
 //    }
-//
+
+    @Test
+    void getUsersById_shouldReturn_whenIdExists() {
+
+        when(modelMapper.map(any(Users.class), eq(UsersDTO.class))).thenReturn(usersDTO);
+        when(usersRepository.existsById(123L)).thenReturn(true);
+        when(usersRepository.findById(123L)).thenReturn(Optional.of(users));
+
+        UsersDTO result = usersService.getUserById(123L);
+
+        assertEquals(123L, result.getId());
+        verify(usersRepository).findById(123L);
+    }
+
+    @Test
+    void getUsersById_shouldNotReturn_whenIdDoesNotExists() {
+
+        when(usersRepository.existsById(12L)).thenReturn(false);
+
+        assertThrows(ResourceNotFoundException.class, () -> usersService.getUserById(12L));
+        verify(usersRepository).existsById(12L);
+    }
+
 //    @Test
-//    void getAuthorById_shouldReturn_whenIdExists() {
+//    void updateUsers_shouldUpdate_whenusersDTOIsValid() {
 //
-//        when(usersRepository.findById(123L)).thenReturn(Optional.of(author));
+//        Users update = new Users();
+//        update.setId(123L);
+//        update.setFirstName("Update");
+//        update.setLastName("Name");
 //
-//        Users result = usersService.getUserById(123L);
-//
-//        assertEquals(123L, result.getId());
-//        verify(usersRepository).findById(123L);
-//    }
-//
-//    @Test
-//    void getAuthorById_shouldNotReturn_whenIdDoesNotExists() {
-//
-//        when(usersRepository.findById(12L)).thenReturn(Optional.empty());
-//
-//        assertThrows(ResourceNotFoundException.class, () -> usersService.getUserById(12L));
-//        verify(usersRepository).findById(12L);
-//    }
-//
-//    @Test
-//    void updateAuthor_shouldUpdate_whenAuthorIsValid() {
-//
-//        Users update = new Users(123L, "Update", "Name");
-//
-//        when(usersRepository.findById(123L)).thenReturn(Optional.of(author));
+//        when(usersRepository.findById(123L)).thenReturn(Optional.of(users));
 //        when(usersRepository.save(any(Users.class))).thenReturn(update);
 //
-//        Users result = usersService.updateUser(update, 123L);
+//        UsersDTO result = usersService.updateUser(modelMapper.map(update, UsersDTO.class), 123L);
 //
 //        assertEquals(123L, result.getId());
 //        assertEquals("Update", result.getFirstName());
@@ -121,12 +149,15 @@
 //    }
 //
 //    @Test
-//    void updateAuthor_shouldNotUpdate_whenIdIsDoesNotExist() {
+//    void updateUsers_shouldNotUpdate_whenIdIsDoesNotExist() {
 //
-//        Users update = new Users(12L, "Update", "Name");
+//        Users update = new Users();
+//        update.setId(12L);
+//        update.setFirstName("Update");
+//        update.setLastName("Name");
 //
-//        assertThrows(ResourceNotFoundException.class, () -> usersService.updateUser(update, 12L));
+//        assertThrows(ResourceNotFoundException.class, () -> usersService.updateUser(modelMapper.map(update, UsersDTO.class), 12L));
 //
 //        verify(usersRepository).findById(12L);
 //    }
-//}
+}
